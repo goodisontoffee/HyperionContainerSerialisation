02c293d9b172605cf7e6773ce2e2d622e3ed6bc8
Stephen Newman <steve@newman.digital>
2017-06-27 11:29:27 +0100

    Illustrating failing test

--- HyperionContainerSerialisation.Tests/ProvisioningActorTests.cs
+++ HyperionContainerSerialisation.Tests/ProvisioningActorTests.cs
@@ -1,30 +1,47 @@
-﻿using Akka.TestKit;
-
-namespace HyperionContainerSerialisation.Tests
+﻿namespace HyperionContainerSerialisation.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Akka.Actor;
+    using Akka.Configuration;
+    using Akka.TestKit.NUnit3;
+    using FluentAssertions;
     using NUnit.Framework;
 
     [TestFixture]
-    public class ProvisioningActorTests
+    public class ProvisioningActorTests : TestKit
     {
+        public ProvisioningActorTests() : base(ConfigurationFactory.ParseString(HOCON.StandardTestHOCON))
+        {
+        }
+
         [Test]
         public void RequestForNamesShouldProvisionTheRightName()
         {
             // Arrange
+            var sut = ActorOf<ProvisioningActor>();
 
             // Act
+            sut.Tell(new NameRequestMessage());
 
             // Assert
+            var result = FishForMessage<ProvisioningResultMessage<IEnumerable<Name>>>(m => true);
+            result.Result.Count().Should().Be(4);
         }
 
         [Test]
         public void RequestForDatesShouldProvisionTheRightDates()
         {
             // Arrange
+            var sut = ActorOf<ProvisioningActor>();
 
             // Act
+            sut.Tell(new DateRequestMessage());
 
             // Assert
+            var result = FishForMessage<ProvisioningResultMessage<IEnumerable<IContainer<DateTime?>>>>(m => true);
+            result.Result.Count().Should().Be(3);
         }
     }
 }
